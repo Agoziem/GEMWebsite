058c73b10c284c4fe18ebf7cd821a3fc8a65d83f
Ndukwe Chiagoziem <chiagoziendukwe90@gmail.com>
2023-06-07 06:26:40 +0100

    changes

--- assets/js/home.js
+++ assets/js/home.js
@@ -5,3 +5,25 @@ var map = new mapboxgl.Map({
   center: [6.800465, 6.124938],
   zoom: 12,
 });
+
+
+// Check if the user is anonymous (you can modify this condition based on your authentication system)
+if (user === 'AnonymousUser') {
+  // Set the time delay for showing the modal (10 minutes = 600000 milliseconds)
+  const timeDelay = 20000;
+
+  // Function to show the modal
+  const showModal = () => {
+    // Select the modal element by its ID
+    const modal = document.getElementById('LoginModal');
+  
+    // Create a Bootstrap Modal instance
+    const modalInstance = new bootstrap.Modal(modal);
+  
+    // Show the modal
+    modalInstance.show();
+  };
+
+  // Set a timeout to trigger the modal after the time delay
+  setTimeout(showModal, timeDelay);
+}
